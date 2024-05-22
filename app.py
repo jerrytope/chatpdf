5076d6371b72e6dbabde93a771ec615ffb5857aa
temitope <jerrytope009@gmail.com>
2024-05-22 15:36:18 +0100

    new

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+# from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -15,13 +16,11 @@ load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_pdf_text(pdf_docs):
+def get_pdf_text(pdf):
     text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    # print("Extracted Text: ", text[:500])
+    pdf_reader = PdfReader(pdf)
+    for page in pdf_reader.pages:
+        text += page.extract_text()
     return text
 
 def get_text_chunks(text):
@@ -33,7 +32,7 @@ def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
-
+    
 def get_conversational_chain():
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
@@ -51,6 +50,8 @@ async def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
+    for i, doc in enumerate(docs):
+        print(f"Doc {i}: {doc.page_content[:500]}")  # Print first 500 characters of each retrieved document for debugging
     chain = get_conversational_chain()
     response = await chain.acall({"input_documents": docs, "question": user_question}, return_only_outputs=True)
     st.write("Reply: ", response["output_text"])
@@ -58,18 +59,21 @@ async def user_input(user_question):
 def main():
     st.set_page_config("Chat PDF")
     st.header("Chat with PDF using Gemini💁")
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    user_question = st.text_input("Ask a Question from the PDF File")
     if user_question:
         asyncio.run(user_input(user_question))
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        pdf_doc = st.file_uploader("Upload your PDF File and Click on the Submit & Process Button", accept_multiple_files=False)
         if st.button("Submit & Process"):
             with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+                if pdf_doc:
+                    raw_text = get_pdf_text(pdf_doc)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+                else:
+                    st.error("Please upload a PDF file.")
 
 if __name__ == "__main__":
     main()
