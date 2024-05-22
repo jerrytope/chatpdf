9f868118171a247529b430c5973a2698e8f1d864
temitope <jerrytope009@gmail.com>
2024-05-22 23:26:24 +0100

    fix

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
-# from langchain.vectorstores import FAISS
+# from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -32,7 +32,7 @@ def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
-    
+
 def get_conversational_chain():
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
@@ -50,8 +50,8 @@ async def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
-    for i, doc in enumerate(docs):
-        print(f"Doc {i}: {doc.page_content[:500]}")  # Print first 500 characters of each retrieved document for debugging
+    # for i, doc in enumerate(docs):
+    #     print(f"Doc {i}: {doc.page_content[:500]}")  # Print first 500 characters of each retrieved document for debugging
     chain = get_conversational_chain()
     response = await chain.acall({"input_documents": docs, "question": user_question}, return_only_outputs=True)
     st.write("Reply: ", response["output_text"])
